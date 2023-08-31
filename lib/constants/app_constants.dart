6720d19ceb441d6b3a8e9221a3080e83d2877e39
Pius <1453344829@qq.com>
2023-09-01 01:10:19 +0800

    first commit

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,8 @@
+abstract class ThingsboardAppConstants {
+  // static final thingsBoardApiEndpoint = 'http://localhost:8080';
+  static final thingsBoardApiEndpoint = 'http://124.223.32.11:8080';
+  static final thingsboardOAuth2CallbackUrlScheme = 'org.thingsboard.app.auth';
+
+  /// Not for production (only for debugging)
+  static final thingsboardOAuth2AppSecret = 'Your app secret here';
+}
